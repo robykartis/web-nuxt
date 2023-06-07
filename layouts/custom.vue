248f12a8618cc134a8c9d5cf3e39b0d5d75ed928
robykartis <robykarti.s@gmail.com>
2023-06-07 17:31:52 +0700

    FIRST

---
+++ layouts/custom.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="CustomLayout">
+        <p>Costum Layout</p>
+        <slot />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="Main">
+        <h1>Index Page</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
